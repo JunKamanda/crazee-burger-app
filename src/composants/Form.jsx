c82686f67d8f0d88e3e44a99b312b813eeee6c61
JunKamanda <jkamanda062@gmail.com>
2025-10-27 08:11:39 +0100

    ✏️  F01: Fixed App.jsx

---
+++ src/composants/Form.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+
+const Form = () => {
+  //State
+  const [prenom, setPrenom] = useState("");
+
+  //comportements
+  const handleChange = (e) => {
+    const valueAfterChange = e.target.value;
+    setPrenom(valueAfterChange);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(`Bonjour ${prenom}`);
+    setPrenom("");
+  };
+  //render
+  return (
+    <div>
+      <form action="submit" onSubmit={handleSubmit}>
+        <input
+          onChange={handleChange}
+          type="text"
+          placeholder="Entrez votre prenom..."
+          value={prenom}
+          required
+        />
+        <button>Accéder à votre espace</button>
+      </form>
+    </div>
+  );
+};
+
+export default Form;
---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,18 @@
+import Form from "../composants/Form";
+
+const LoginPage = () => {
+  //state
+
+  //comportements
+
+  //render
+  return (
+    <div>
+      <h1>Bienvenue chez nous</h1>
+      <h2>connectez-vous</h2>
+      <Form/>
+    </div>
+  );
+};
+
+export default LoginPage;
