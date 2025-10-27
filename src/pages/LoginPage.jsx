d4c25b0cc562e918a894080fa52699b9d08f8163
JunKamanda <jkamanda062@gmail.com>
2025-10-27 09:20:34 +0100

    ✨ F01: LOGIN > Add title and form

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -2,6 +2,7 @@ import FormLogin from "../composants/FormLogin";
 
 const LoginPage = () => {
   //state
+  
 
   //comportements
 
@@ -9,6 +10,7 @@ const LoginPage = () => {
   return (
     <div>
       <h1>Bienvenue chez nous</h1>
+      <br />
       <h2>connectez-vous</h2>
       <FormLogin/>
     </div>
