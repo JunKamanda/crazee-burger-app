594d4ac4e27d4fb03639163b98d594fe4f16c44a
JunKamanda <jkamanda062@gmail.com>
2025-11-01 13:17:43 +0100

    ✨ F02: ORDERPAGE > create route

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const ErrorPage = () => {
-    return (
-        <div>
-            <h1>Not Found</h1>
-        </div>
-    );
+  //state
+  const navigate = useNavigate();
+
+  //comportement
+  const handleClick = () => {
+    navigate("/");
+  };
+
+  //render
+  return (
+    <div>
+      <h1>Page Not Found</h1>
+      <button onClick={handleClick}>Retourner vers la page d'accueil</button>
+    </div>
+  );
 };
 
 export default ErrorPage;
--- src/pages/OrderPage.jsx
+++ src/pages/OrderPage.jsx
@@ -1,18 +1,25 @@
-import React from 'react';
-import { useLocation } from 'react-router-dom';
+import React from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const OrderPage = () => {
-    //state
-    const location =  useLocation()
-    const prenom =  location.state?.prenom || "inconnu";
-    //comportement
+  //state
+  const location = useLocation();
+  const prenom = location.state?.prenom || "inconnu";
+  const navigate = useNavigate();
 
-    //render
-    return (
-        <div>
-            <h1>Bonjour {prenom}</h1>
-        </div>
-    );
+  //comportement
+  const handleClick = ()=>{
+    navigate("/")
+  }
+
+  //render
+  return (
+    <div>
+      <h1>Bonjour {prenom}</h1>
+      <br />
+      <button onClick={handleClick}>Déconnexion</button>
+    </div>
+  );
 };
 
 export default OrderPage;
